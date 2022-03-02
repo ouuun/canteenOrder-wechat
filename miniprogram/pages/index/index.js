8824514091b0d0ffaae52a2fcc61a78c5891a9dd
ouuun <954574569@qq.com>
2022-03-02 14:12:02 +0800

    feat(index):基础页面设计

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,10 +1,26 @@
-// index.js
-// const app = getApp()
-const { envList } = require('../../envList.js');
-
 Page({
-  data: {
-    envList,
-    selectedEnv: envList[0],
+  data:{
+    index:0,
+  },
+  login: function(event) {
+    // wx.login({
+    //   success (res) {
+    //     if (res.code) {
+    //       console.log(res.code);
+    //     } else {
+    //       console.log('登录失败！' + res.errMsg)
+    //     }
+    //   }
+    // });
+    wx.getUserProfile({
+      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res);
+        // this.setData({
+        //   userInfo: res.userInfo,
+        //   hasUserInfo: true
+        // })
+      }
+    })
   }
 });

2a8fb049a33de37376a35b66883dffbc3902eb7d
ouuun <954574569@qq.com>
2022-03-08 17:27:55 +0800

    test(cuisine):测试拖拽排序

--- miniprogram/pages/cuisine/cuisine.js
+++ miniprogram/pages/cuisine/cuisine.js
@@ -2,6 +2,7 @@ let app = getApp()
 app.unitgameinfo = { "members": [{ "member": { "nickname": "小程序照片合成", "job": "ckext" }, }, { "member": { "nickname": "高球丸子" }, }, { "member": { "nickname": "DRobertdsf", "job": "黄金" }, }, { "member": { "nickname": "erer", "job": "ckext" }, }, { "member": { "nickname": "just do it", "job": "黄金" }, }, { "member": { "nickname": "888" }, }], };
 Page({
   data: {
+    isMove: false,
   },
   onLoad: function (options) {
     var info = app.unitgameinfo, list;
@@ -13,7 +14,9 @@ Page({
         this.getwxmlcode("#movelist0", (res) => {
           this.setData({ movelist0: res })
           var jiange = res.top - resp.top;
+          console.log('jiange----->', jiange);
           var yiban = res.bottom - res.top + jiange;
+          console.log('yiban----->', yiban);
           this.setData({
             itemheight: res.bottom - res.top,
             jiange: yiban, //两条中间到另一条的距离
@@ -29,24 +32,28 @@ Page({
     query1.select(str).boundingClientRect()
     query1.selectViewport().scrollOffset()
     query1.exec((res) => {
+      console.log(`${str}-------->`, res);
       if (cal) cal(res[0])
     })
   },
   listitemmove(e) {
-    // console.log(e)
     if (e.type == "touchmove") {
       var movetop = e.touches[0].pageY - this.data.itemheight;
       var moveoutindex = parseInt((movetop - this.data.jianqu) / this.data.jiange);
       if (e.currentTarget.dataset.index <= moveoutindex) moveoutindex++;
       this.moveoutindex = moveoutindex;
-      this.setData({ nowmoveindex: e.currentTarget.dataset.index, movetop, moveoutindex })
+      this.setData({ nowmoveindex: e.currentTarget.dataset.index, movetop, moveoutindex });
+      this.setData({ isMove: true });
     } else {
-      let index = e.currentTarget.dataset.index, score = this.data.list;
-      let data = { ...score[index] };
-      score.splice(index, 1);
-      if (index <= this.moveoutindex - 1) this.moveoutindex--;
-      score.splice(this.moveoutindex, 0, data);
-      this.setData({ list: score, moveoutindex: -1, nowmoveindex: -1 });
+      if (this.data.isMove) {
+        let index = e.currentTarget.dataset.index, score = this.data.list;
+        let data = { ...score[index] };
+        score.splice(index, 1);
+        if (index <= this.moveoutindex - 1) this.moveoutindex--;
+        score.splice(this.moveoutindex, 0, data);
+        this.setData({ list: score, moveoutindex: -1, nowmoveindex: -1 });
+        this.setData({ isMove: false });
+      };
     }
   },
   onShow: function () {
@@ -54,5 +61,8 @@ Page({
   lastsubmit() {
     console.log(this.data.list)
   },
+  test: function () {
+    console.log(123);
+  }
 
 })

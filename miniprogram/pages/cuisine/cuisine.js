2e6e5491ffe6822fef61e60d755cb769833714fe
ouuun <954574569@qq.com>
2022-03-18 17:13:30 +0800

    feat(cuisine):调整拖拽排序

--- miniprogram/pages/cuisine/cuisine.js
+++ miniprogram/pages/cuisine/cuisine.js
@@ -1,22 +1,21 @@
 let app = getApp()
-app.unitgameinfo = { "members": [{ "member": { "nickname": "小程序照片合成", "job": "ckext" }, }, { "member": { "nickname": "高球丸子" }, }, { "member": { "nickname": "DRobertdsf", "job": "黄金" }, }, { "member": { "nickname": "erer", "job": "ckext" }, }, { "member": { "nickname": "just do it", "job": "黄金" }, }, { "member": { "nickname": "888" }, }], };
 Page({
   data: {
+    typeList: [{ "name": "热销", "order": "1" }, { "name": "必点", "order": "2" }, { "name": "热销", "order": "1" }, { "name": "必点", "order": "2" }],
     isMove: false,
   },
   onLoad: function (options) {
-    var info = app.unitgameinfo, list;
-    list = info.members;
-    this.setData({ options, info, list });
+    this.initDrag();
+
+  },
+  initDrag: function (params) {
     this.getwxmlcode("#movebox", (resp) => {
       this.setData({ movebox: resp })
       setTimeout(() => {
         this.getwxmlcode("#movelist0", (res) => {
           this.setData({ movelist0: res })
           var jiange = res.top - resp.top;
-          console.log('jiange----->', jiange);
           var yiban = res.bottom - res.top + jiange;
-          console.log('yiban----->', yiban);
           this.setData({
             itemheight: res.bottom - res.top,
             jiange: yiban, //两条中间到另一条的距离
@@ -25,9 +24,8 @@ Page({
         })
       }, 300)
     })
-
   },
-  getwxmlcode(str, cal) {
+  getwxmlcode: function (str, cal) {
     const query1 = wx.createSelectorQuery()
     query1.select(str).boundingClientRect()
     query1.selectViewport().scrollOffset()
@@ -36,22 +34,31 @@ Page({
       if (cal) cal(res[0])
     })
   },
-  listitemmove(e) {
+  move: function (e) {
     if (e.type == "touchmove") {
       var movetop = e.touches[0].pageY - this.data.itemheight;
       var moveoutindex = parseInt((movetop - this.data.jianqu) / this.data.jiange);
       if (e.currentTarget.dataset.index <= moveoutindex) moveoutindex++;
       this.moveoutindex = moveoutindex;
       this.setData({ nowmoveindex: e.currentTarget.dataset.index, movetop, moveoutindex });
+
+      this.animate(`#movelist${e.currentTarget.dataset.index}`, [
+        {
+          top: movetop + 'px',
+        },
+      ],
+        0,
+      )
+
       this.setData({ isMove: true });
     } else {
       if (this.data.isMove) {
-        let index = e.currentTarget.dataset.index, score = this.data.list;
+        let index = e.currentTarget.dataset.index, score = this.data.typeList;
         let data = { ...score[index] };
         score.splice(index, 1);
         if (index <= this.moveoutindex - 1) this.moveoutindex--;
         score.splice(this.moveoutindex, 0, data);
-        this.setData({ list: score, moveoutindex: -1, nowmoveindex: -1 });
+        this.setData({ typeList: score, moveoutindex: -1, nowmoveindex: -1 });
         this.setData({ isMove: false });
       };
     }

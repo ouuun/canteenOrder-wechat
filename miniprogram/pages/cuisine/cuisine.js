6e8fd591b9d6ddacd67679a67d6b6269de38df7c
ouuun <954574569@qq.com>
2022-03-30 16:20:45 +0800

    feat(cuisine):完善类型页管理

--- miniprogram/pages/cuisine/cuisine.js
+++ miniprogram/pages/cuisine/cuisine.js
@@ -1,13 +1,33 @@
+const { request, needLogin } = require('../../util/http/request')
 let app = getApp()
 Page({
   data: {
-    typeList: [{ "name": "热销", "order": "1" }, { "name": "必点", "order": "2" }, { "name": "热销", "order": "1" }, { "name": "必点", "order": "2" }],
+    typeList: [],
     isMove: false,
     sort: false,
+    addShow: false,
+    editShow: false,
+    newType: '',
+    editType: '',
+    editId: 0,
   },
-  onLoad: function (options) {
-    this.initDrag();
+  onLoad: async function (options) {
+    await needLogin();
+    await this.getTypeList();
+    if (this.data.typeList?.length > 0) {
+      this.initDrag();
+    }
   },
+  getTypeList: async function () {
+    const res = await request({
+      url: '/api/manager/type/search',
+      method: 'GET',
+    });
+    this.setData({
+      typeList: res.data.list.sort((a, b) => { return a.sort - b.sort })
+    });
+  },
+  // 初始化拖动排序所需信息
   initDrag: function (params) {
     this.getwxmlcode("#movebox", (resp) => {
       this.setData({ movebox: resp })
@@ -30,46 +50,119 @@ Page({
     query1.select(str).boundingClientRect()
     query1.selectViewport().scrollOffset()
     query1.exec((res) => {
-      console.log(`${str}-------->`, res);
       if (cal) cal(res[0])
     })
   },
+  //当触发移动
   move: function (e) {
     if (e.type == "touchmove") {
       var movetop = e.touches[0].pageY - this.data.itemheight;
       var moveoutindex = parseInt((movetop - this.data.jianqu) / this.data.jiange);
       if (e.currentTarget.dataset.index <= moveoutindex) moveoutindex++;
-      this.moveoutindex = moveoutindex;
+      // this.moveoutindex = moveoutindex;
       this.setData({ nowmoveindex: e.currentTarget.dataset.index, movetop, moveoutindex });
-
-      this.animate(`#movelist${e.currentTarget.dataset.index}`, [
-        {
-          top: movetop + 'px',
-        },
-      ],
-        0,
-      )
-
+      this.animate(`#movelist${e.currentTarget.dataset.index}`, [{ top: movetop + 'px' }], 0);
       this.setData({ isMove: true });
     } else {
       if (this.data.isMove) {
-        let index = e.currentTarget.dataset.index, score = this.data.typeList;
+        let index = e.currentTarget.dataset.index;
+        let score = this.data.typeList;
         let data = { ...score[index] };
         score.splice(index, 1);
-        if (index <= this.moveoutindex - 1) this.moveoutindex--;
-        score.splice(this.moveoutindex, 0, data);
+        if (index <= this.data.moveoutindex - 1) this.data.moveoutindex--;
+        score.splice(this.data.moveoutindex, 0, data);
         this.setData({ typeList: score, moveoutindex: -1, nowmoveindex: -1 });
         this.setData({ isMove: false });
+        this.data.typeList.forEach((item, index) => {
+          item.sort = index + 1
+        });
       };
     }
   },
-  sort: function () {
+  //排序完成
+  sort: async function () {
     if (this.data.sort) {
-      console.log('保存')
+      console.log('保存');
+      const sorted = this.data.typeList.map(item => { return { id: item.id, sort: item.sort } });
+      const res = await request({
+        url: '/api/manager/type/sort',
+        method: 'POST',
+        data: {
+          sorted: sorted,
+        },
+        token: true
+      });
+      if (res.code == 200) {
+        await this.getTypeList();
+        this.onClose();
+      }
     }
     this.setData({
       sort: !this.data.sort,
     })
+  },
+  //添加
+  add() {
+    this.setData({ addShow: true });
+  },
+  //点击编辑按钮
+  edit(event) {
+    const data = event.currentTarget.dataset;
+    this.setData({ editShow: true, editType: data.name, editId: data.id });
+  },
+  //关闭
+  onClose() {
+    this.setData({ addShow: false, editShow: false });
+  },
+  //新增type
+  addType: async function () {
+    const res = await request({
+      url: '/api/manager/type/add',
+      method: 'POST',
+      data: {
+        name: this.data.newType,
+        sort: this.data.typeList.length + 1
+      },
+      token: true
+    });
+    if (res.code == 200) {
+      this.setData({
+        newType: ''
+      });
+      await this.getTypeList();
+      this.onClose();
+    }
+  },
+  // 删除
+  deleteType: async function () {
+    const res = await request({
+      url: '/api/manager/type/delete',
+      method: 'POST',
+      data: {
+        id: this.data.editId,
+      },
+      token: true
+    });
+    if (res.code == 200) {
+      await this.getTypeList();
+      this.onClose();
+    }
+  },
+  // 修改
+  updateType: async function () {
+    var list = this.data.typeList;
+    const res = await request({
+      url: '/api/manager/type/update',
+      method: 'POST',
+      data: {
+        id: this.data.editId,
+        name: this.data.editType
+      },
+      token: true
+    });
+    if (res.code == 200) {
+      await this.getTypeList();
+      this.onClose();
+    }
   }
-
 })
--- miniprogram/util/http/request.js
+++ miniprogram/util/http/request.js
@@ -1,21 +1,31 @@
 const config = require('../config/config')
 
 module.exports = {
-    request: function ({ url, method, data = {}, token }) {
+    request: async function ({ url, method, data = {}, token }) {
         wx.showLoading({
             title: '加载中',
-            mask:true
+            mask: true
         });
         let header = { "Content-type": "application/json", };
         if (token) {
             if (wx.getStorageSync('token')) {
                 header = Object.assign(header, { Authorization: 'Bearer ' + wx.getStorageSync('token') })
+            } else {
+                wx.hideLoading();
+                wx.showModal({
+                    title: '提示',
+                    content: '请登录',
+                    success(res) {
+                        if (res.confirm) wx.navigateTo({ url: '/pages/index/index' });
+                    }
+                })
+                return null;
             }
         }
         return new Promise((resolve, reject) => {
             wx.hideLoading();
             wx.request({
-                url: `${config.host}`+url,
+                url: `${config.host}` + url,
                 method: method,
                 data: data,
                 header: header,
@@ -33,11 +43,22 @@ module.exports = {
                     wx.showToast({
                         title: error.errMsg,
                         icon: 'none',
-                        duration:2000
+                        duration: 2000
                     })
                     // reject(error.data)
                 }
             })
+        })
+    },
+    needLogin: async function () {
+        if (wx.getStorageSync('token')) return;
+        wx.showModal({
+            title: '提示',
+            content: '请登录',
+            success(res) {
+                if (res.confirm) wx.navigateTo({ url: '/pages/index/index' });
+                wx.navigateBack()
+            }
         })
     }
 }
